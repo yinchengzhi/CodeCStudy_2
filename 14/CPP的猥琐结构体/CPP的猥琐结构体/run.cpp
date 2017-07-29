bf4b049ea77d3f65de3c6a44b9a736699f740423
Valar Morghulis <524672142@qq.com>
2017-07-29 18:20:47 +0800

    update

---
+++ 14/CPP的猥琐结构体/CPP的猥琐结构体/run.cpp
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+struct MyStruct{
+	int a = 10;				//C++结构体变量可以初始化
+	MyStruct() {			//CPP
+
+	}
+	int go() {				//可以包含函数
+
+	}
+};
+
+void run(int a) {			//C++ 根据参数区别函数
+
+}
+
+void run() {
+
+}
+
+void main() {
+
+
+
+}
