b40357bce89b40926648facb2e62f500a0fba531
Valar Morghulis <524672142@qq.com>
2017-08-21 23:55:44 +0800

    update

--- 15/全局变量与局部变量/全局变量与局部变量/局部变量案例.c
+++ 15/全局变量与局部变量/全局变量与局部变量/局部变量案例.c
@@ -2,20 +2,20 @@
 #include<stdlib.h>
 
 void fun(int x, int y) {			//函数的副本机制，重新构建的变量
-	int z = 10;
-	x += 10;
-	y += 10;
+	int z = 10;						//z = 10
+	x += 10;						//x = 20
+	y += 10;						//y = 20
 	if (z) {
-		int z = 20;
-		printf("x = %d y =%d z = %d\n", x, y, z);
+		int z = 20;					//屏蔽，同名的情况下，块语句内部的变量生效
+		printf("x = %d y =%d z = %d\n", x, y, z);			//20,20,20
 	}
-	printf("x = %d y = %d z = %d\n", x, y, z);
+	printf("x = %d y = %d z = %d\n", x, y, z);			//20,20,10
 }
 
 void main() {
 	int x = 10, y = 10, z = 10;
 	fun(x, y);						//函数除非传地址，否则因为副本机制不会改变x,y
-	printf("x = %d y = %d z = %d\n", x, y, z);
+	printf("x = %d y = %d z = %d\n", x, y, z);			//10,10,10
 
 	system("pause");
 }
